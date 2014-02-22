5757d34adb293445de3754059faa9bff222c057f
Claudio Wilson <claudiowilson@utexas.edu>
2014-02-22 10:02:22 -0600

    initial commit of teaser

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
-var http = require('http');
-http.createServer(function(req, res) {
-    res.end('<h1>Hello, world.</h1>');
-}).listen(10001);
+var express = require('express'),
+	port = 10001;
+var app = express();
+app.configure(function() {
+	app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+});
+
+app.get('/', function(request, response) {
+	response.render('index.jade');
+});
+
+app.get('/test', function(request, response) {
+	response.render('companioncube.jade');
+})
+
+app.listen(port);
+console.log("Listening on " + port);
